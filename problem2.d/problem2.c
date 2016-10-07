af06602f2144cf84d89a07290fe01561dc8bdf01
Tanner Grehawick <tgrehawi@gmail.com>
2016-10-07 02:41:30 -0400

    started problem 2

---
+++ problem2.d/problem2.c
@@ -0,0 +1,11 @@
+// Author: Tanner Grehawick
+//	Email: tgrehawi@gmail.com
+// Problem 1: Overlapping Segments
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char **argv) {
+	return 0;
+}
