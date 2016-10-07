9620a95d20a2dbf376ef9040c6bddf34194be916
Tanner Grehawick <tgrehawi@gmail.com>
2016-10-07 03:49:30 -0400

    did some work
    + parsing pairs of strings and ignoring comments
    + declaring operators as both an enum and an array of strings using preprocessor shenanigans

--- problem2.d/problem2.c
+++ problem2.d/problem2.c
@@ -1,11 +1,67 @@
 // Author: Tanner Grehawick
 //	Email: tgrehawi@gmail.com
-// Problem 1: Overlapping Segments
+// Problem 2: Stack-based Computer Architecture Simulation
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+#ifndef _INCLUDE
+	#define startops typedef enum
+	#define op(name) name,
+	#define endops ops
+#else
+	#undef startops
+	#undef op
+	#undef endops
+	#define startops const char *opnames[OPS_COUNT] =
+	#define op(name) #name,
+	#define endops
+#endif
+
+#ifndef _INCLUDE
+
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <ctype.h>
+	#include <stdint.h>
+
+	#define OPS_COUNT 16
+	typedef int32_t number;
+
+#endif
+
+startops {
+	op(PUSH) op(POP) op(DUPE) op(PEEK)
+	op(ADD) op(SUB) op(MUL) op(DIV)
+	op(ROT) op(BRANCH) op(CALL) op(BRZ)
+	op(BGZ) op(BLZ) op(RETURN) op(HCF)
+} endops;
+
+#ifndef _INCLUDE
+
+	#define _INCLUDE
+	#include "problem2.c"
+
+
+#define skipspace() while (*s && isspace(*s)) s++
+#define parsestr() while (*s && !isspace(*s)) *(b++) = *(s++); *b = 0
 
 int main(int argc, char **argv) {
+	size_t n = 0;
+	char *line;
+	char *s, *b;
+	char buf1[32], buf2[32];
+	ssize_t status = getline(&line, &n, stdin);
+	while (status > 0) {
+		s = line;
+		while (*s && *s != '#') s ++;
+		if (*s == '#') *s = 0;
+		s = line;
+		skipspace();
+		b = buf1; parsestr();
+		skipspace();
+		b = buf2; parsestr();
+		status = getline(&line, &n, stdin);
+	}
+	free(line);
 	return 0;
 }
+
+#endif
