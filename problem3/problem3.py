1178fb71d04ac08c8dd19d28753b7e4b745deaa1
Tanner Grehawick <tgrehawi@gmail.com>
2016-10-21 02:32:27 -0400

    finished parser

--- problem3/problem3.py
+++ problem3/problem3.py
@@ -6,55 +6,63 @@ import re
 # Author: Tanner Grehawick
 # Email: tgrehawi@gmail.com
 # Problem 3: Navigating The Flash
-for line in stdin:
-	print map(lambda o: int(o), re.compile(r'(\d+)').findall(line))
-exit(0)
 
-class InputError(RuntimeError):
-	__init__
-class Pair:
-	pattern = re.compile(r'\s*(\d+)\s+(\d+)')
-	def __init__(this, r, c):
-		this.r = int(r) or 0
-		this.c = int(c) or 0
+class Pair(object):
+
+	__pattern = re.compile(r'\s*(\d+)\s+(\d+)')
+	__slots__ = ('r', 'c')
+
+	def __init__(self, r, c):
+		self.r = int(r) or 0
+		self.c = int(c) or 0
+
 	@classmethod
 	def read(cls, file):
 		line = file.readline()
-		if line:
-			m = Pair.pattern.match(line)
-			if not m:
-				raise FormatError()
+		m = cls.__pattern.match(line)
+		if m:
 			return cls(*m.group(1, 2))
-		else:
-			raise EOFError("")
-	def __repr__(this):
-		return "(%d, %d)" % (this.r, this.c)
+
+	def __repr__(self):
+		return "(%d, %d)" % (self.r, self.c)
+
 class Grid:
-	def __init__(this, rcount, ccount, rowsource = None):
-		this.vals = []
-		for r in range(0, rcount):
-			row = rowsource and rowsource(r) or []
-			this.vals.append(row)
+
+	__pattern = re.compile(r'(\d+)')
+
+	def __init__(self, rcount, ccount, rowproducer = None):
+		rp = rowproducer
+		self.rows = [
+			rp and rp(r) or [None for c in xrange(ccount)]
+			for r in xrange(rcount)
+		]
+
 	@classmethod
 	def read(cls, file):
 		size = Pair.read(file)
-		grid = cls(size.r, size.c, lambda r: ))
-	class __RowReader:
-		pattern = re.compile(r'\s*(\d+)')
-		def __init__(this, file):
-			this.file = file
-		def __call__(this, r):
-			line = this.file.readline()
-			if line:
-				return map(lambda o: int(o), this.pattern.findall(line))
-			else:
-				raise EOFError("")
+		return cls(
+			size.r, size.c,
+			lambda r: map(
+				int, cls.__pattern.findall(file.readline())
+			)
+		)
+
+	def __repr__(self):
+		return '\n'.join(
+			repr(row) for row in self.rows
+		)
+
+grid = Grid.read(stdin)
 start = Pair.read(stdin)
 ends = []
-end = Pair.read(stdin)
-try:
-	while True:
-		ends.append(Pair.read(stdin))
-except EOFError:
-	pass
-print ends
+while True:
+	end = Pair.read(stdin)
+	if end:
+		ends.append(end)
+	else:
+		break
+print "grid:\t", repr(grid).replace("\n", "\n\t")
+print "start:\t", start
+print "ends:",
+for end in ends:
+	print "\t", end
